90642fdeb97522caf3dd2a8e6107cffc98ef14d4
Richelle de Macêdo Monteiro <richellemonteiro@gmail.com>
2025-04-28 21:17:38 -0300

    atualizaçao Prof e Disciplina

--- api/src/main/java/br/com/alunoonline/api/repository/DisciplinaRepository.java
+++ api/src/main/java/br/com/alunoonline/api/repository/DisciplinaRepository.java
@@ -1,1 +1,7 @@
+package br.com.alunoonline.api.repository;
 
+import br.com.alunoonline.api.model.Disciplina;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DisciplinaRepository extends JpaRepository <Disciplina, Long> {
+}
