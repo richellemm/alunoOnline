482850b496096366b71b2e9658e7236e08ab83b2
Richelle de Macêdo Monteiro <richellemonteiro@gmail.com>
2025-05-20 21:32:48 -0300

    Historico

--- api/src/main/java/br/com/alunoonline/api/controller/MatriculaAlunoController.java
+++ api/src/main/java/br/com/alunoonline/api/controller/MatriculaAlunoController.java
@@ -1,5 +1,7 @@
 package br.com.alunoonline.api.controller;
 
+import br.com.alunoonline.api.dtos.AtualizarNotasRequestDTO;
+import br.com.alunoonline.api.dtos.HistoricoAlunoResponseDTO;
 import br.com.alunoonline.api.model.MatriculaAluno;
 import br.com.alunoonline.api.service.MatriculaAlunoService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,4 +28,16 @@ public class MatriculaAlunoController {
         matriculaAlunoService.trancarMatricula(id);
     }
 
+    @PatchMapping("/atualizar-notas/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void atualizarNotas(@PathVariable Long id, @RequestBody AtualizarNotasRequestDTO atualizarNotasRequestDTO){
+        matriculaAlunoService.atualizarNotas(id, atualizarNotasRequestDTO);
+
+    }
+
+    @GetMapping("/emitir-historico/{alunoId}")
+    @ResponseStatus(HttpStatus.OK)
+    public HistoricoAlunoResponseDTO emitirHistorico(@PathVariable Long alunoId){
+       return matriculaAlunoService.emitirHistorico(alunoId);
+    }
 }
--- api/src/main/java/br/com/alunoonline/api/service/MatriculaAlunoService.java
+++ api/src/main/java/br/com/alunoonline/api/service/MatriculaAlunoService.java
@@ -1,6 +1,8 @@
 package br.com.alunoonline.api.service;
 
 import br.com.alunoonline.api.dtos.AtualizarNotasRequestDTO;
+import br.com.alunoonline.api.dtos.DisciplinasAlunoResponseDTO;
+import br.com.alunoonline.api.dtos.HistoricoAlunoResponseDTO;
 import br.com.alunoonline.api.enums.MatriculaAlunoStatusEnum;
 import br.com.alunoonline.api.model.MatriculaAluno;
 import br.com.alunoonline.api.repository.MatriculaAlunoRepository;
@@ -8,6 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class MatriculaAlunoService {
@@ -38,36 +43,72 @@ public class MatriculaAlunoService {
         }
     }
 
-        public void atualizarNotas(Long matriculaAlunoId,
-                AtualizarNotasRequestDTO atualizarNotasRequestDTO){
-            MatriculaAluno matriculaAluno = buscarMatriculaOuLancarExcecao(matriculaAlunoId);
-            if (atualizarNotasRequestDTO.getNota1() != null){
-                matriculaAluno.setNota1(atualizarNotasRequestDTO.getNota1());
+    public void atualizarNotas(Long matriculaAlunoId,
+                               AtualizarNotasRequestDTO atualizarNotasRequestDTO) {
+        MatriculaAluno matriculaAluno = buscarMatriculaOuLancarExcecao(matriculaAlunoId);
+        if (atualizarNotasRequestDTO.getNota1() != null) {
+            matriculaAluno.setNota1(atualizarNotasRequestDTO.getNota1());
 
-            }
-
-            if(atualizarNotasRequestDTO.getNota2() != null){
-                matriculaAluno.setNota2(atualizarNotasRequestDTO.getNota2());
-            }
-
-            calcularMediaEModificarStatus(matriculaAluno);
-            matriculaAlunoRepository.save(matriculaAluno);
         }
 
-        private MatriculaAluno buscarMatriculaOuLancarExcecao(Long matriculaAlunoId){
-            return matriculaAlunoRepository.findById(matriculaAlunoId)
-                    .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
-                            "Matrícula do aluno não encontrada."));
+        if (atualizarNotasRequestDTO.getNota2() != null) {
+            matriculaAluno.setNota2(atualizarNotasRequestDTO.getNota2());
         }
 
-        private void calcularMediaEModificarStatus(MatriculaAluno matriculaAluno){
-            Double nota1 = matriculaAluno.getNota1();
-            Double nota2 = matriculaAluno.getNota2();
-
-            if (nota1 != null && nota2 != null){
-                    Double media = (nota1 + nota2) / QTD_NOTAS;
-                    matriculaAluno.setStatus(media >= MEDIA_PARA_APROVACAO ? MatriculaAlunoStatusEnum.APROVADO : MatriculaAlunoStatusEnum.REPROVADO);
+        calcularMediaEModificarStatus(matriculaAluno);
+        matriculaAlunoRepository.save(matriculaAluno);
     }
 
+    public HistoricoAlunoResponseDTO emitirHistorico(Long alunoId) {
+        List<MatriculaAluno> matriculaAlunos = matriculaAlunoRepository.findByAlunoId(alunoId);
+
+        if (matriculaAlunos.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Esse aluno não possui matrículas");
+
         }
+
+        HistoricoAlunoResponseDTO historicoAluno = new HistoricoAlunoResponseDTO();
+        historicoAluno.setNomeAluno(matriculaAlunos.get(0).getAluno().getNome());
+        historicoAluno.setEmailAluno(matriculaAlunos.get(0).getAluno().getEmail());
+        historicoAluno.setCpfAluno(matriculaAlunos.get(0).getAluno().getCpf());
+
+        List<DisciplinasAlunoResponseDTO> disciplinas = matriculaAlunos.stream()
+                .map(this::mapearParaDisciplinasAlunoResponseDTO)
+                .toList();
+
+        historicoAluno.setDisciplinas(disciplinas);
+        return historicoAluno;
+    }
+
+    private MatriculaAluno buscarMatriculaOuLancarExcecao(Long matriculaAlunoId) {
+        return matriculaAlunoRepository.findById(matriculaAlunoId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+                        "Matrícula do aluno não encontrada."));
+    }
+
+    private void calcularMediaEModificarStatus(MatriculaAluno matriculaAluno) {
+        Double nota1 = matriculaAluno.getNota1();
+        Double nota2 = matriculaAluno.getNota2();
+
+        if (nota1 != null && nota2 != null) {
+            Double media = (nota1 + nota2) / QTD_NOTAS;
+            matriculaAluno.setStatus(media >= MEDIA_PARA_APROVACAO ? MatriculaAlunoStatusEnum.APROVADO : MatriculaAlunoStatusEnum.REPROVADO);
+        }
+    }
+
+    private DisciplinasAlunoResponseDTO mapearParaDisciplinasAlunoResponseDTO(MatriculaAluno matriculaAluno) {
+        DisciplinasAlunoResponseDTO response = new DisciplinasAlunoResponseDTO();
+        response.setNomeDisciplina(matriculaAluno.getDisciplina().getNome());
+        response.setNomeProfessor(matriculaAluno.getDisciplina().getProfessor().getNome());
+        response.setNota1(matriculaAluno.getNota1());
+        response.setNota2(matriculaAluno.getNota2());
+        response.setMedia(calcularMedia(matriculaAluno.getNota1(), matriculaAluno.getNota2()));
+        response.setStatus(matriculaAluno.getStatus());
+
+        return response;
+    }
+
+    private Double calcularMedia(Double nota1, Double nota2){
+        return (nota1 != null && nota2 !=null) ? (nota1 + nota2) / QTD_NOTAS : null;
+    }
 }
